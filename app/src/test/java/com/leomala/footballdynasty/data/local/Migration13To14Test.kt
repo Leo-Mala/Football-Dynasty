7b152b731efd9b216930b5c09df68044e0eaf697
Márcio Leandro de Almeida Malaquias <leomalaj10@gmail.com>
2026-09-07 20:11:48 -0300

    test: extend V13 migration probe through V17

--- app/src/test/java/com/leomala/footballdynasty/data/local/Migration13To14Test.kt
+++ app/src/test/java/com/leomala/footballdynasty/data/local/Migration13To14Test.kt
@@ -62,6 +62,7 @@ class Migration13To14Test {
                 Phase15JuniorDraftMigration.MIGRATION_13_14,
                 Phase15SeniorRuntimeMigration.MIGRATION_14_15,
                 Phase16CompetitionPlayerRatingMigration.MIGRATION_15_16,
+                Phase17LegacyDurabilityMigration.MIGRATION_16_17,
             )
             .build()
 
