db1e1b6101564106086028f016421459fb930c62
Márcio Leandro de Almeida Malaquias <leomalaj10@gmail.com>
2026-09-07 20:11:29 -0300

    test: extend V12 migration probe through V17

--- app/src/test/java/com/leomala/footballdynasty/data/local/Migration12To13Test.kt
+++ app/src/test/java/com/leomala/footballdynasty/data/local/Migration12To13Test.kt
@@ -64,6 +64,7 @@ class Migration12To13Test {
                 Phase15JuniorDraftMigration.MIGRATION_13_14,
                 Phase15SeniorRuntimeMigration.MIGRATION_14_15,
                 Phase16CompetitionPlayerRatingMigration.MIGRATION_15_16,
+                Phase17LegacyDurabilityMigration.MIGRATION_16_17,
             )
             .build()
         val row = requireNotNull(migrated.careerCompetitionDao().findCompetition(CAREER, "league-1"))
